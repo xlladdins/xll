27da4f90db60bb77ffed26d55de85d195ad5df68
Keith A. Lewis <kal@kalx.net>
2020-07-19 00:59:10 -0400

    project template

--- setup.bat
+++ setup.bat
@@ -1,1 +1,3 @@
 rem Run this to install xll related files
+xcopy Visualizers\*.* %USERPROFILE%Documents\Visual Studio 2019\Visualizers
+xcopy xllproject.zip %USERPROFILE%Documents\Visual Studio 2019\Templates\ProjectTemplates\
