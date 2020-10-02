973456fc2e150f29fdde10db40c7d2d54208cc54
Keith A. Lewis <kal@kalx.net>
2020-10-02 19:24:27 -0400

    Alert help file needs !0 suffix

--- test/test.cpp
+++ test/test.cpp
@@ -20,7 +20,7 @@ int WINAPI xll_macro(void)
 			Excel(xlfReftext, Excel(xlfActiveCell), OPER(true))
 		),
 		OPER(2), // general information
-		OPER("https://xlladdins.github.io/Excel4Macros/ALERT.html") // help
+		OPER("https://xlladdins.github.io/Excel4Macros/alert.html!0") // help
 	);
 
 	return TRUE;
