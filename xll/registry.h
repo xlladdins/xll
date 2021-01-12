8c6d1e54eaaf23a08d82ccc8306ec85b20f21112
Keith A. Lewis <kal@kalx.net>
2021-01-11 23:45:31 -0500

    registry cleanup

--- xll/registry.h
+++ xll/registry.h
@@ -1,6 +1,20 @@
 // registry.h - Windows registry wrappers
 #include <Windows.h>
 #include <stdexcept>
+#include <string>
+
+namespace Win {
+
+	inline std::string GetFormatMessage(DWORD id)
+	{
+		char buf[1024] = "unknown error";
+		DWORD size = 1024;
+		FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, 0, id, 0, buf, size, 0);
+
+		return std::string(buf);
+	}
+
+}
 
 namespace Reg {
 	class CreateKey {
@@ -63,4 +77,80 @@ namespace Reg {
 			return Proxy(*this, value);
 		}
 	};
+
+	template<class T>
+	inline typename T QueryValue(HKEY hKey, PCTSTR lpValue);
+	
+	template<>
+	inline DWORD QueryValue<DWORD>(HKEY hKey, PCTSTR lpValue)
+	{
+		DWORD value;
+		DWORD type = REG_DWORD;
+
+		LSTATUS status = RegQueryValueEx(hKey, lpValue, 0, &type, (LPBYTE)&value, 0);
+		if (ERROR_SUCCESS != status) {
+			throw Win::GetFormatMessage(status);
+		}
+
+		return value;
+	}
+	
+	template<>
+	inline std::basic_string<TCHAR> QueryValue<std::basic_string<TCHAR>>(HKEY hKey, PCTSTR lpValue)
+	{
+		std::basic_string<TCHAR> value;
+		DWORD type = REG_SZ;
+		DWORD data = 0;
+
+		LSTATUS status = RegQueryValueEx(hKey, lpValue, NULL, &type, NULL, &data);
+		if (ERROR_SUCCESS != status) {
+			throw Win::GetFormatMessage(status);
+		}
+		size_t n = data / sizeof(TCHAR);
+		value.resize(n);
+		status = RegQueryValueEx(hKey, lpValue, NULL, &type, (LPBYTE)value.data(), &data);
+		if (ERROR_SUCCESS != status) {
+			throw Win::GetFormatMessage(status);
+		}
+
+		return value.erase(value.find('\0'));
+	}
+
+	template<class T>
+	inline typename T GetValue(HKEY hKey, PCTSTR lpSubKey, PCTSTR lpValue = 0);
+
+	template<>
+	inline DWORD GetValue<DWORD>(HKEY hKey, PCTSTR lpSubKey, PCTSTR lpValue)
+	{
+		DWORD value;
+		DWORD type = REG_DWORD;
+
+		LSTATUS status = RegGetValue(hKey, lpSubKey, lpValue, RRF_RT_REG_DWORD, &type, (LPBYTE)&value, 0);
+		if (ERROR_SUCCESS != status) {
+			throw Win::GetFormatMessage(status);
+		}
+
+		return value;
+	}
+
+	template<>
+	inline std::basic_string<TCHAR> GetValue<std::basic_string<TCHAR>>(HKEY hKey, PCTSTR lpSubKey, PCTSTR lpValue)
+	{
+		std::basic_string<TCHAR> value;
+		DWORD type = REG_SZ;
+		DWORD data = 0;
+
+		LSTATUS status = RegGetValue(hKey, lpSubKey, lpValue, RRF_RT_REG_SZ, &type, NULL, &data);
+		if (ERROR_SUCCESS != status) {
+			throw Win::GetFormatMessage(status);
+		}
+		size_t n = data / sizeof(TCHAR);
+		value.resize(n);
+		status = RegGetValue(hKey, lpSubKey, lpValue, RRF_RT_REG_SZ, &type, (LPBYTE)value.data(), &data);
+		if (ERROR_SUCCESS != status) {
+			throw Win::GetFormatMessage(status);
+		}
+
+		return value.erase(value.find('\0'));
+	}
 }
