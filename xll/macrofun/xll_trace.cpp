e6ec72350cd8a4356d17ccf882d050e6c660987c
Keith A. Lewis <kal@kalx.net>
2021-03-01 17:02:45 -0500

    Add TRACE function

---
+++ xll/macrofun/xll_trace.cpp
@@ -0,0 +1,31 @@
+// trace.cpp - trace Excel calculation
+#include "../xll.h"
+
+using namespace xll;
+
+static AddIn xai_trace(
+    Function(XLL_LPXLOPER, "xll_trace", "TRACE")
+    .Arguments({
+        Arg(XLL_LPXLOPER, "cell", "is the cell to trace."),
+    })
+    .Category("XLL")
+    .FunctionHelp("Alert when cell is called in a calculation.")
+    .Uncalced()
+);
+LPXLOPERX WINAPI
+xll_trace(LPXLOPERX px)
+{
+#pragma XLLEXPORT
+    if (px->xltype == xltypeSRef) {
+        OPER s;
+
+        s =  OPER("Reference: ") & Excel(xlfReftext, *px, OPER(true));
+        s &= OPER("\nFormula: ") & Excel(xlfGetCell, OPER(6), *px);
+        s &= OPER("\nContents: ") & Excel(xlfText, Excel(xlfGetCell, OPER(5), *px), OPER("General"));
+        
+        Excel(xlcAlert, s, OPER(2)); // info
+    }
+
+    return px;
+}
+
