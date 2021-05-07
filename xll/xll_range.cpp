b7de2cb0fe6f00f09a55f21e7dfe7641c2f81a97
Keith A. Lewis <keithalewis@live.com>
2021-05-06 23:17:09 -0400

    range

--- xll/xll_range.cpp
+++ xll/xll_range.cpp
@@ -4,14 +4,16 @@
 using namespace xll;
 
 AddIn xai_range_set(
-	Function(XLL_HANDLEX, "xll_range_set_", "\\RANGE.SET")
+	Function(XLL_HANDLEX, "xll_range_set_", "\\RANGE")
 	.Arguments({
 		Arg(XLL_LPOPER, "range", "is the range to set.", "{1,2;3,4}")
 		})
 	.Uncalced()
 	.FunctionHelp("Return a handle to a range.")
 	.Category("XLL")
-	//.Documentation(R"(Create a handle to a two dimensional range of cells)")
+	.Documentation(R"(
+Create a handle to a two dimensional range of cells.
+)")
 );
 HANDLEX WINAPI xll_range_set_(LPOPER px)
 {
@@ -28,12 +30,14 @@ AddIn xai_range_get(
 	})
 	.FunctionHelp("Return the range held by a handle.")
 	.Category("XLL")
-	//.Documentation(R"(Return a two dimensional range of cells)")
+	.Documentation(R"(
+Return a two dimensional range of cells.
+)")
 );
 LPOPER WINAPI xll_range_get(HANDLEX h)
 {
 #pragma XLLEXPORT
-	handle<OPER> h_(h, false);
+	handle<OPER> h_(h);
 
 	if (!h_) {
 		XLL_ERROR("RANGE.GET: unknown handle");
