be8ab793f270157abcb69afbaf6c18eff788d88f
Keith A. Lewis <kal@kalx.net>
2021-06-24 17:42:09 -0400

    decode from string

--- xll/codec.h
+++ xll/codec.h
@@ -3,81 +3,24 @@
 #pragma once
 #include <concepts>
 #include <iostream>
-#include "traits.h"
+#include "excel.h"
 
 namespace xll {
 
-
-	template<class X, class C = traits<X>::xchar>
-	std::basic_ostream<C>& encode(std::basic_ostream<C>& os, const X& x)
+	template<class X>
+	inline XOPER<X> decode(const typename traits<X>::xcstr str, DWORD len = 0)
 	{
-		// for xltypeMulti
-		static C lb = '{', rb = '}', fs = ',', rs = ';';
+		XLOPER<X> o = len == 0 ? XOPER<X>(str) : XOPER<X>(str, len);
+		
+		// convert to number, date, boolean, or error
+		XLOPER<X> o_ = XExcel<X>(xlfEvaluate, o);
 
-		switch (x.xltype & xlbitmask) {
-		case xltypeNum:
-			os << x.val.num;
-			break;
-		case xltypeStr:
-			os.write(x.val.str + 1, x.val.str[0]);
-			break;
-		case xltypeErr:
-			os << xll_err_desc[x.val.err];
-			break;
-		case xltypeMulti:
-			os << rb;
-			for (unsigned i = 0; i < rows(x); ++i) {
-				if (i > 0) {
-					os << rs;
-				}
-				for (unsigned j = 0; j < columns(x); ++j) {
-					if (j > 0) {
-						os << fs;
-					}
-					os << index(x, i, j);
-				}
-			}
-			os << lb;
-			break;
-		case xltypeSRef:
-			X o;
-			int result = traits<X>::Excelv(xlfReftext, &o, 1, &x);
-			ensure(result == xlretSuccess and o.val.xltype == xltypeStr);
-			os.write(o.val.str + 1, o.val.str[0]);
-			traits<X>::Excelv(xlFree, 0, 1, &o);
-			break;
-		case xltypeRef:
-			ensure(!"not implemented");
-			break;
-		case xltypeInt:
-			os << x.val.w;
-			break;
-		case xltypeBool:
-			os << x.val.xbool != 0;
-			break;
-		default:
-			ensure(!"unknown type");
+		// error strings evaluate to xltypeErr
+		if (!o_.is_err() or o == XOPER<X>(xll_err_str[o_.val.err])) {
+			o = o_;
 		}
 
-		return os;
+		return o;
 	}
 
-	template<class X, class C = traits<X>::xchar>
-	inline X decode(std::basic_istream<C>& is)
-	{
-		C c = 0; // length char at front
-		std::basic_ostringstream<C> os;
-		os << c;
-		os << is;
-		std::basic_string<C> s(os.str());
-		s[0] = static_cast<C>(s.length());
-		X x, o;
-		x.xltype = xltypeStr;
-		x.val.str = &s[0];
-		int result = traits<X>::Excelv(xlfEvaluate, &o, 1, &x);
-		ensure(result == xlretSuccess);
-		x.xlbit |= xlbitXLFree;
-
-		return x;
-	}
 }
